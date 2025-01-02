49c209ab8705e2fd61c6e51d09aa4793c6c5b31e
Gregorio Carrillo <gregorio.carrillo@gmail.com>
2025-01-02 12:22:08 -0400

    primer commit

---
+++ HelloWorld.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, World!\n");
+
+    int x;       // This is a variable declaration
+    x = 123;     // This is a variable assignment
+    int y = 321; // This is a variable declaration and assignment
+
+    int age = 47;         // integer
+    float gpa = 10.9;     // floating point number
+    char grade = 'C';     // single character
+    char name[] = "Greg"; // string
+
+    printf("Hello %s\n", name);
+    printf("You are %d years old\n", age);
+    return 0;
+}
